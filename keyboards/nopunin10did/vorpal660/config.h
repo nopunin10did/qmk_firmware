155df68e1e0e09a0f286de984b379f2d6c9af436
NoPunIn10Did <w.alex.ronke@gmail.com>
2022-01-12 12:11:08 -0500

    Update core configuration files

--- keyboards/nopunin10did/vorpal660/config.h
+++ keyboards/nopunin10did/vorpal660/config.h
@@ -22,20 +22,19 @@
 #define VENDOR_ID           0x4E50  // "NP"
 #define PRODUCT_ID          0x5642  // "V" 66
 #define DEVICE_VER          0x0001
-#define PRODUCT             Vorpal660
+#define PRODUCT             Vorpal 660
 
 /* key matrix size */
-#define MATRIX_ROWS         12
-#define MATRIX_COLS         10
+#define MATRIX_ROWS         10
+#define MATRIX_COLS         8
 
 /* key matrix pins */
-#define MATRIX_ROW_PINS     { E6, B4, B5, B7, D5, C7, F1, F0, B1, B3, B2, B6 }
-#define MATRIX_COL_PINS     { F4, F5, F6, F7, D7, C6, D4, D0, D2, D3 }
+#define MATRIX_ROW_PINS     { D5, D3, D2, B0, B6, B5, B4, D7, D6, D4 }
+#define MATRIX_COL_PINS     { F0, F1, F4, F5, F6, F7, C7, B7 }
 #define UNUSED_PINS
 
 /* Indicator LEDs */
-#define LED_NUM_LOCK_PIN    D1
-#define LED_CAPS_LOCK_PIN   B0
+#define LED_CAPS_LOCK_PIN   C6
 
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION     COL2ROW
@@ -43,11 +42,7 @@
 /* Set 0 if debouncing isn't needed */
 #define DEBOUNCE            5
 
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-#define LOCKING_SUPPORT_ENABLE
+/* Rotary encoder rotation pins */
+#define ENCODERS_PAD_A      { D0 }
+#define ENCODERS_PAD_B      { D1 }
 
-/* Locking resynchronize hack */
-#define LOCKING_RESYNC_ENABLE
-
-/* Reducing layer count to 3 for via support */
-#define DYNAMIC_KEYMAP_LAYER_COUNT 3
