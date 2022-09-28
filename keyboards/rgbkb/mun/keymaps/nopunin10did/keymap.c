5382bcf4e59377d107f9a0caf7f0cd717dca2230
nopunin10did <w.alex.ronke@gmail.com>
2022-09-27 22:49:33 -0400

    Minor change

--- keyboards/rgbkb/mun/keymaps/nopunin10did/keymap.c
+++ keyboards/rgbkb/mun/keymaps/nopunin10did/keymap.c
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_RBRC,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_BSLS,  
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_EQL,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,  KC_ENT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    LSADEL,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  RSDASH,   KC_RSFT,
-        KC_LCTL, KC_LGUI, KC_LALT, ADJUST,  FN,      KC_SPC,  KC_SPC,    ALGRHOM, ALGREND, KC_LEFT, KC_RIGHT,KC_DOWN , KC_UP,    RCBE
+        KC_LCTL, KC_LGUI, KC_LALT, ADJUST,  FN,      KC_SPC,  KC_SPC,    ALGRHOM, ALGRHOM, KC_LEFT, KC_RIGHT,KC_DOWN , KC_UP,    RCBE
     ),
 
     [_FN] = LAYOUT(
