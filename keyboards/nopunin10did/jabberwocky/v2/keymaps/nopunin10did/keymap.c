52106d18a58a17b7116675aa4f2ab94fb4a4d742
NoPunIn10Did <w.alex.ronke@gmail.com>
2024-12-03 13:20:29 -0500

    Update Jabberwocky personal keymap

--- keyboards/nopunin10did/jabberwocky/v2/keymaps/nopunin10did/keymap.c
+++ keyboards/nopunin10did/jabberwocky/v2/keymaps/nopunin10did/keymap.c
@@ -28,7 +28,7 @@
 #define FN_PENT LT(1,KC_PENT)
 #define CTALDEL LCA(KC_DEL)
 #define JPYEN   KC_INT3
-#define ALTESC  LALT_T(KC_ESC)
+#define ALT_GRV LALT_T(KC_GRV)
 #define SCRSHOT RSG(KC_S)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -36,11 +36,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [0] = LAYOUT_rh(
 
 KC_ESC ,KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,KC_F6  ,                KC_F7  ,KC_F8  ,KC_F9  ,KC_F10 ,KC_F11 ,KC_F12 ,KC_HOME,KC_END ,KC_PGUP,KC_PGDN,KC_DEL ,KC_INS ,KC_PSCR,KC_BSPC,    
-        KC_GRV ,KC_1   ,KC_2   ,KC_3   ,KC_4   ,KC_5   ,                KC_6   ,KC_7   ,KC_8   ,KC_9   ,KC_0   ,KC_MINS,KC_EQL ,KC_BSPC,KC_BSPC,        KC_EQL ,KC_PSLS,KC_PAST,KC_PMNS,    
+        KC_ESC ,KC_1   ,KC_2   ,KC_3   ,KC_4   ,KC_5   ,                KC_6   ,KC_7   ,KC_8   ,KC_9   ,KC_0   ,KC_MINS,KC_EQL ,KC_BSPC,KC_BSPC,        KC_EQL ,KC_PSLS,KC_PAST,KC_PMNS,    
         KC_TAB ,KC_Q   ,KC_W   ,KC_E   ,KC_R   ,KC_T   ,                KC_Y   ,KC_U   ,KC_I   ,KC_O   ,KC_P   ,KC_LBRC,KC_RBRC,KC_BSLS,                KC_P7  ,KC_P8  ,KC_P9  ,KC_PPLS,     
         KC_LCTL,KC_A   ,KC_S   ,KC_D   ,KC_F   ,KC_G   ,                KC_H   ,KC_J   ,KC_K   ,KC_L   ,KC_SCLN,KC_QUOT,KC_ENT ,KC_ENT ,                KC_P4  ,KC_P5  ,KC_P6  ,KC_PPLS,     
 KC_LSFT,KC_LSFT,KC_Z   ,KC_X   ,KC_C   ,KC_V   ,KC_B   ,LSA_DEL,FN_CALC,KC_N   ,KC_M   ,KC_COMM,KC_DOT ,KC_SLSH,KC_RSFT,KC_RSFT,KC_UP  ,                KC_P1  ,KC_P2  ,KC_P3  ,FN_PENT,     
-        KC_LCTL,KC_LGUI,        ALTESC ,FN_MUTE,KC_SPC ,                SHAGHOM,        RALTHOM,        RWINEND,RCTLEND,KC_LEFT,KC_DOWN,KC_RGHT,        KC_P0  ,KC_P0  ,KC_PDOT,FN_PENT),   
+        KC_LCTL,KC_LGUI,        ALT_GRV,FN_MUTE,KC_SPC ,                SHAGHOM,        RALTHOM,        RWINEND,RCTLEND,KC_LEFT,KC_DOWN,KC_RGHT,        KC_P0  ,KC_P0  ,KC_PDOT,FN_PENT),   
     
     
 [1] = LAYOUT_rh(
